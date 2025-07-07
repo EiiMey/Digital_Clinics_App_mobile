bf36097a6c264d0b0648d657d835882e5f9a630a
KRIS <eiimeycambodia24@gmail.com>
2025-07-08 01:56:43 +0700

    New feature

--- lib/profiles_layout/personal_account.dart
+++ lib/profiles_layout/personal_account.dart
@@ -88,14 +88,120 @@ class PersonalAccount extends StatelessWidget {
                       children: [
                         Row(
                           children: [
-                            CircleAvatar(
-                              child: ElevatedButton(
-                                child: Image.asset('assets/images/Avatar.png'),
-                                onPressed: () {
-                                }, 
+                            Container(
+                              width: 80,
+                              height: 80,
+                              child: CircleAvatar(
+                                backgroundImage: AssetImage(
+                                  'assets/images/Avatar.png',
+                                ),
                               ),
                             ),
-                            Column(children: []),
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  GestureDetector(
+                                    child: Text('USER NAME',style: TextStyle(fontFamily: 'Poppins-Regular', fontSize: 24, ),),
+                                    onTap: () {},
+                                  ),
+                                  SizedBox(height: 5,),
+                                  GestureDetector(
+                                    child: Text('Year old', style: TextStyle(fontFamily: 'Poppins-Regular', fontSize: 14),),
+                                    onTap: () {},
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20,),
+                        Divider(),
+                        SizedBox(height: 20,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    color: Colors.indigo.shade300,
+                                    borderRadius: BorderRadius.circular(50)
+                                  ),
+                                  child: Icon(Icons.person_outline, size: 28,),
+                                ),
+                                SizedBox(height: 10,),
+                                GestureDetector(
+                                  child: Text(' Saved\nDoctors', style: TextStyle(
+                                    fontFamily: 'Poppins-Regular',
+                                    color: Colors.grey.shade600,
+                                    fontSize: 16,
+                                    ),),
+                                  onTap: () {
+                                    
+                                  },
+                                )
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    color: Colors.lightBlue.shade300,
+                                    borderRadius: BorderRadius.circular(50)
+                                  ),
+                                  child: Icon(Icons.article_outlined, size: 28,),
+                                ),
+                                SizedBox(height: 10,),
+                                GestureDetector(
+                                  child: Text(' Saved\nArticles', style: TextStyle(
+                                    fontFamily: 'Poppins-Regular',
+                                    color: Colors.grey.shade600,
+                                    fontSize: 16,
+                                    ),),
+                                  onTap: () {
+                                    
+                                  },
+                                )
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    color: Colors.green.shade300,
+                                    borderRadius: BorderRadius.circular(50)
+                                  ),
+                                  child: Icon(Icons.favorite_border, size: 28,),
+                                ),
+                                SizedBox(height: 10,),
+                                GestureDetector(
+                                  child: Text('Health\n Diary', style: TextStyle(
+                                    fontFamily: 'Poppins-Regular',
+                                    color: Colors.grey.shade600,
+                                    fontSize: 16,
+                                    ),),
+                                  onTap: () {
+                                    
+                                  },
+                                )
+                              ],
+                            ),
                           ],
                         ),
                       ],
