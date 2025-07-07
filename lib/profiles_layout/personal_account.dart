b730fee3aa6d8e2397964c8571923b434d19a5eb
KRIS <eiimeycambodia24@gmail.com>
2025-07-08 01:11:54 +0700

    New feature

--- lib/profiles_layout/personal_account.dart
+++ lib/profiles_layout/personal_account.dart
@@ -1,20 +1,107 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Personal.Account',
+      home: PersonalAccount(),
+    ),
+  );
+}
+
 class PersonalAccount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo[50],
       body: Stack(
         children: [
           SingleChildScrollView(
-            child: Column(
+            child: Stack(
               children: [
-                Text('Hello world'),
-                IconButton(onPressed: () {
-                  
-                },
-                icon: Icon(Icons.add_box_outlined)
-                )
+                Container(
+                  width: double.infinity,
+                  height: 320,
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  decoration: BoxDecoration(color: Color(0xFF6673CC)),
+                  child: Stack(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 60),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Title(
+                              color: Colors.white,
+                              child: Text(
+                                'Profile',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Poppins-Semibold',
+                                  fontSize: 28,
+                                ),
+                              ),
+                            ),
+                            Spacer(),
+                            Container(
+                              child: Row(
+                                children: [
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.notifications_none_rounded,
+                                      color: Colors.white,
+                                      size: 26,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.settings_outlined,
+                                      color: Colors.white,
+                                      size: 26,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 120),
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: Container(
+                    width: double.infinity,
+                    height: 400,
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              child: ElevatedButton(
+                                child: Image.asset('assets/images/Avatar.png'),
+                                onPressed: () {
+                                }, 
+                              ),
+                            ),
+                            Column(children: []),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
